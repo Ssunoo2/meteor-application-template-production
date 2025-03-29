edd4c4b64ab6e444f201d36d3c8a38dfe2f4907d
Sean Sunoo <97641529+Ssunoo2@users.noreply.github.com>
2025-03-28 15:04:59 -1000

    added new collection initilization for larger JSON files

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -3,6 +3,40 @@ import { Stuffs } from '../../api/stuff/StuffCollection';
 /* eslint-disable no-console */
 
 // Initialize the database with a default data document.
+function loadJSON(filePath) {
+  console.log(`Attempting to load JSON from: ${filePath}`);
+  try {
+    // eslint-disable-next-line no-undef
+    const jsonData = Assets.getText(filePath);
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error(`Error loading JSON from ${filePath}: ${error}`);
+    return null;
+  }
+}
+
+function initializeCollection(collection, defaultData, collectionName) {
+  if (collection.count() === 0 && defaultData) {
+    console.log(`Creating default ${collectionName}.`);
+    defaultData.forEach(data => {
+      try {
+        collection.define(data);
+        console.log(`Added: ${data.name} to ${collectionName}`);
+      } catch (error) {
+        console.error(`Error adding ${data.name} to ${collectionName}: ${error}`);
+      }
+    });
+  }
+}
+
+if (Meteor.isServer) {
+  const testData = loadJSON('test.json');
+  console.log(testData);
+
+  // const defaultClasses = loadJSON('data/defaultClasses.json');
+  // initializeCollection(classescollection, defaultClasses, 'classes');
+}
+
 function addData(data) {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Stuffs.define(data);
